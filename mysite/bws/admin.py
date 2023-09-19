d7313824bc237440999c9443f4fc74bc4379e8da
Arnold Voiskunovic <voiskunovic@gmail.com>
2023-09-19 16:53:12 +0300

    Papildyta i seller order produkto atvaizduojamas kiekis

--- mysite/bws/admin.py
+++ mysite/bws/admin.py
@@ -9,6 +9,17 @@ class ProductAdmin(admin.ModelAdmin):
 
 class SellerOrderAdmin(admin.ModelAdmin):
     form = SellerOrderForm
+    readonly_fields = ('get_capacity_calculation',)
+
+    def get_capacity_calculation(self, obj):
+        if obj.buyer_order and obj.buyer_order.product:
+            product = obj.buyer_order.product
+            return product.capacity_calculation()
+        else:
+            return "No Capcity Calculation"
+
+    get_capacity_calculation.short_description = 'Capacity Calculation'
+
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Buyer)
